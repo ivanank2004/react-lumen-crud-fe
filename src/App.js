5c493ddf7713a69769d4faf914f836d0213cf8f8
ivanank2004 <ivan.anugerah.k@gmail.com>
2025-09-25 09:16:31 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    api.get('/').then(res => setMsg(res.data.message));
+  }, []);
+
+  return <h1>{msg}</h1>;
 }
-
-export default App;
